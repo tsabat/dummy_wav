a196e2e8bdefce5792593e8aaa0b48fd45026435
Daria Neznaeva <darianeznaeva@gmail.com>
2022-02-15 19:38:04 -0500

    using dynamic column names and DictReader

--- wave_writer.py
+++ wave_writer.py
@@ -20,43 +20,46 @@ def write_beep(file_name: Path, duration=5.0):
 @click.option(
     "--csv-name",
     default="example.csv",
-    help="Type the file name of the csv file, please",
+    help="The name of the CSV file to be read in order to generate the WAV files",
 )
 @click.option(
     "--file-name-column",
-    default=0,
-    help="Write the column number with the file names",
+    default="File Name",
+    help="The name of the column that stores the expected name of the file",
 )
 @click.option(
     "--file-length-column",
-    default=1,
-    help="Write the column number with the file lengths",
+    default="Duration",
+    help="The name of the column that stores the expected duration of the file",
 )
 def run(csv_name, file_name_column, file_length_column):
-    row_number=0
     is_valid=True
     rows=[]
+
+    # parse, validate, and modify the data as needed
     with open(csv_name) as csvfile:
-        reader = csv.reader(
+        reader = csv.DictReader(
             csvfile,
         )
+        row_number=0
         for row in reader:
             row_number+=1
-            if type(row[file_name_column]) != str:
-                print(f'Row number {row_number} has a wrong type. File name column contains {row[file_name_column]}.')
-                is_valid = False
-                break
+
+            row[file_name_column] = row[file_name_column].strip()
+
             try:
-                float(row[file_length_column])
+                row[file_length_column] = float(row[file_length_column])
                 rows.append(row)
             except:
                 print(f'Row number {row_number} has a wrong type. Length column contains {row[file_length_column]}.')
                 is_valid = False
-                break  
+                break
+
+    # then, use the data
     if is_valid == True:
         for row in rows:
-            file_name = row[file_name_column].strip()
-            duration = float(row[file_length_column])
+            file_name = row[file_name_column]
+            duration = row[file_length_column]
             print(f"writing {duration} s to {file_name}")
             write_beep(file_name=file_name, duration=duration)
 
