5f23c515e178237efc7fd47848c9bead61296a8e
Timothy Sabat <timothysabat@Timothys-MBP.localdomain>
2022-02-17 20:07:06 -0800

    8 bit 100hz

--- library.py
+++ library.py
@@ -10,21 +10,25 @@ WAV_EXTENSION = ".wav"
 
 def parse_validate_and_clean(csv_name, file_name_column, file_length_column, debug):
     is_valid = True
-    rows=[]
+    rows = []
     with open(csv_name) as csvfile:
         reader = csv.DictReader(
             csvfile,
         )
-        row_number=0
+        row_number = 0
         seen_file_names = set()
         for row in reader:
-            row_number+=1
+            row_number += 1
             if not file_name_column in row:
-                print(f"A column named {file_name_column} does not exist. Please enter a different column name and try again")
+                print(
+                    f"A column named {file_name_column} does not exist. Please enter a different column name and try again"
+                )
                 is_valid = False
                 break
             if not file_length_column in row:
-                print(f"A column named {file_length_column} does not exist. Please enter a different column name and try again")
+                print(
+                    f"A column named {file_length_column} does not exist. Please enter a different column name and try again"
+                )
                 is_valid = False
                 break
             clean_file_name = row[file_name_column].strip()
@@ -33,7 +37,9 @@ def parse_validate_and_clean(csv_name, file_name_column, file_length_column, deb
                     print(f"Adding {WAV_EXTENSION} to {clean_file_name}")
                 clean_file_name = f"{clean_file_name}{WAV_EXTENSION}"
             if clean_file_name in seen_file_names:
-                print(f'Row number {row_number} has a duplicate filename, {clean_file_name}. Please rename and try again')
+                print(
+                    f"Row number {row_number} has a duplicate filename, {clean_file_name}. Please rename and try again"
+                )
                 is_valid = False
                 break
             seen_file_names.add(clean_file_name)
@@ -43,7 +49,9 @@ def parse_validate_and_clean(csv_name, file_name_column, file_length_column, deb
                 row[file_length_column] = float(row[file_length_column])
                 rows.append(row)
             except:
-                print(f'Row number {row_number} has an invalid value. Duration column contains {row[file_length_column]}. Please enter a number and try again')
+                print(
+                    f"Row number {row_number} has an invalid value. Duration column contains {row[file_length_column]}. Please enter a number and try again"
+                )
                 is_valid = False
                 break
     return is_valid, rows
@@ -55,11 +63,18 @@ def write_beeps(rows, file_name_column, file_length_column, debug):
         duration = row[file_length_column]
         if debug:
             print(f"Creating {file_name} with {duration} second duration")
+        # write_sine(file_name=file_name, duration=duration)
         write_beep(file_name=file_name, duration=duration)
 
 
-def write_beep(file_name: Path, duration=5.0):
-    f = 440  # sine frequency, Hz, may be float
-    fs = 22050
-    samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.uint8)
-    wavfile.write(file_name, 22050, samples)
+def write_beep(
+    file_name: Path,
+    duration=5.0,
+):
+    # https://www.colincrawley.com/audio-file-size-calculator/
+    f = 80  # sine frequency, Hz, may be float
+    sample_rate_hz = 100
+    samples = (
+        np.sin(2 * np.pi * np.arange(sample_rate_hz * duration) * f / sample_rate_hz)
+    ).astype(np.uint8)
+    wavfile.write(file_name, sample_rate_hz, samples)
--- wave_writer.py
+++ wave_writer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import click
 
-from library import parse_validate_and_clean, write_beeps
+from library import parse_validate_and_clean, write_beeps, write_beep
 
 
 @click.command()
@@ -23,17 +23,28 @@ from library import parse_validate_and_clean, write_beeps
 @click.option(
     "-d",
     "--debug",
-    default=False, 
+    default=False,
     is_flag=True,
     help="Whether or not to show debug output",
 )
 def run(csv_name, file_name_column, file_length_column, debug):
-    is_valid, rows = parse_validate_and_clean(csv_name, file_name_column, file_length_column, debug)
+    is_valid, rows = parse_validate_and_clean(
+        csv_name, file_name_column, file_length_column, debug
+    )
 
     # then, use the data
     if is_valid == True:
         write_beeps(rows, file_name_column, file_length_column, debug)
 
 
+def make_a_lot(count):
+    assert count > 0, "count must be > 0"
+    while count > 0:
+        print(count)
+        write_beep(f"test/f{count}.wav", 15)
+        count -= 1
+
+
 if __name__ == "__main__":
     run()
+    # make_a_lot(10000)
